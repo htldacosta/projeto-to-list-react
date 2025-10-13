4941c41ac38d5f7e2d38ac9f3d88db181407ec0d
htldacosta <hitaludcosta@gmail.com>
2025-10-13 14:16:29 -0400

    projeto finalizado

--- meu-todo-list/src/App.jsx
+++ meu-todo-list/src/App.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-import "./App.css"; // Importando o CSS
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    // { text: "Task teste", isCompleted: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1); //
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Minha Lista de Tarefas</h1>
